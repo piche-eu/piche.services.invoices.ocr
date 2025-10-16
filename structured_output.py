f13aaf28126da9260ad1516ec9d77ba10ffe5815
Leonid Didukh <ledidukh@gmail.com>
2025-10-16 14:09:03 +0300

    provided structured output

---
+++ structured_output.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+import dspy
+
+
+class Date(BaseModel):
+    year: int
+    month: int
+    day: int
+    hour: int
+
+
+class InvoiceData(dspy.Signature):
+    invoiceNumber: str = dspy.OutputField(desc="invoice number")
+    invoiceDate: Date = dspy.OutputField(desc="Date when invoice was issued")
+    seller: str = dspy.OutputField(desc="The name of the seller")
+    sellerRegNumber: int = dspy.OutputField(desc="Seller registration number")
+    buyer: str = dspy.OutputField(desc="The name of buyer")
+    buyerRegNumber: int = dspy.OutputField(desc="Buyer registration number")
+    paymentDeadline: Date = dspy.OutputField(
+        desc="Deadline of payment mantioned in the invoice"
+    )
+    netPrice: float = dspy.OutputField(desc="Net price")
+    vat: float = dspy.OutputField(desc="VAT price")
+    totalPrice: float = dspy.OutputField(desc="Total price")
+    currency: str = dspy.OutputField(desc="Currency")
+    summary: str = dspy.OutputField(desc="Summary")
+    # filename:str = dspy.InputField();
+    filecontent: str = dspy.InputField()
+    prompt: str = dspy.InputField()
+    # error:str = dspy.InputField();
